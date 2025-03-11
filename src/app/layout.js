b994727ef376f6096f89b034002f41b9c5b0687d
RishabhRV4444 <rishabh@arevei.com>
2025-03-11 20:34:59 +0530

    change title

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "SGTMake: Your One-Stop Shop for Electronic Components",
+  title: "SGTMake: Your One-Stop Shop for Engineering Services",
   description: "SGTMake is an online store that caters to a wide range of electronic product needs. They offer a comprehensive selection of components and assemblies, making them an ideal resource for various industries and individual projects.",
   keywords: ["sgtmake electronic components", "3D Designing Services", "3D Printing Services"," CNC Machining Services", "Laser Cutting Services", "Custom Battery Pack Manufacturing", "Wiring Harness Manufacturing"," Prototyping Services" ,"Precision Engineering","Custom Manufacturing Solutions","Rapid Prototyping","Product Development Services","Reverse Engineering Services","CAD Design Services","Plastic Fabrication Services","Metal Fabrication Services","Custom Component Manufacturing"]
 };
